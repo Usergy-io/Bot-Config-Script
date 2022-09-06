7db5f3d106233983e6c038c163df5cc677512e56
teo <teodor@calin.ro>
2022-09-06 02:44:55 +0200

    202209060244-S1
    Fix Script
    Add user

---
+++ adduser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Enter username: "
+read username
+useradd "$username"
+echo "temp4now" | passwd --stdin "$username"
+chage -d 0 "$username"
--- config.sh
+++ config.sh
@@ -29,25 +29,6 @@ sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script\n"
 
 printf "\n";
-
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
-
-
 printf "Updating apt...\n";
 sudo apt update
 printf "\n";
@@ -87,20 +68,6 @@ printf "\n";
 tar xzf ./actions-runner-linux-x64-2.296.1.tar.gz 
 printf "\n";
 printf "Runner extracted!\n";
-printf "\n";
-printf "Copying runner to /home/username/";
-printf "\n";
-sudo cp -R ./actions-runner /home/$username/
-printf "\n";
-printf "Runner copied!\n";
-printf "\n";
-su - $username
-printf "\n";
-printf "Creating sudo user...\n";
-printf "\n";
-sudo useradd -m $username
-printf "\n";
-printf "Sudo user created!\n";
 printf "\n";
 printf "Process done, continue to activte the runner in github.\n";
 exit 0
